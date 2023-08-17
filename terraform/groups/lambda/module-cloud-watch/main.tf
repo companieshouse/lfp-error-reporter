07da521c08027e9431c9f4f0f222af1790c03676
Mike Kelly <mkelly1@companieshouse.gov.uk>
2023-08-17 12:03:11 +0100

    Update structure of terraform work for dev/staging/live.

--- terraform/groups/lambda/module-cloud-watch/main.tf
+++ terraform/groups/lambda/module-cloud-watch/main.tf
@@ -1,13 +1,13 @@
 resource "aws_cloudwatch_event_rule" "lfp_error_reporter" {
   name        = "${var.service}-${var.environment}"
   description = "Call lfp error reporter lambda"
-  schedule_expression ="${var.cron_schedule}"
+  schedule_expression =var.cron_schedule
 }
 
 resource "aws_cloudwatch_event_target" "call_lfp_error_reporter_lambda" {
-    rule = "${aws_cloudwatch_event_rule.lfp_error_reporter.name}"
+    rule = aws_cloudwatch_event_rule.lfp_error_reporter.name
     target_id = "${var.service}-${var.environment}"
-    arn = "${var.lambda_arn}"
+    arn = var.lambda_arn
 }
 
 resource "aws_lambda_permission" "allow_cloudwatch_to_call_lfp_error_reporter" {
@@ -15,5 +15,5 @@ resource "aws_lambda_permission" "allow_cloudwatch_to_call_lfp_error_reporter" {
     action = "lambda:InvokeFunction"
     function_name = "${var.service}-${var.environment}"
     principal = "events.amazonaws.com"
-    source_arn = "${aws_cloudwatch_event_rule.lfp_error_reporter.arn}"
+    source_arn = aws_cloudwatch_event_rule.lfp_error_reporter.arn
 }
