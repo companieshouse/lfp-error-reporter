7d9f71a109b2c63d61c882e034723e8c92f07b8f
Mike Kelly <mkelly1@companieshouse.gov.uk>
2023-09-08 12:52:48 +0100

    Debugging.

--- dao/mongo.go
+++ dao/mongo.go
@@ -55,27 +55,35 @@ func (m *Mongo) GetLFPData(reconciliationMetaData *models.ReconciliationMetaData
 
 	var penaltiesData models.PenaltyList
 
-	log.Info("MK: GetLFPData: len(penalties) => " + strconv.Itoa(len(penalties)))
-	log.Info("MK: GetLFPData: penaltiesData.Penalties[0].CompanyNumber => " + penaltiesData.Penalties[0].CompanyNumber)
-	log.Info("MK: GetLFPData: penaltiesData.Penalties[0].E5CommandError => " + penaltiesData.Penalties[0].E5CommandError)
-	log.Info("MK: GetLFPData: penaltiesData.Penalties[0].Reference => " + penaltiesData.Penalties[0].Reference)
-	log.Info("MK: GetLFPData: penaltiesData.Penalties[0]..Data.Payment.Amount => " + penaltiesData.Penalties[0].Data.Payment.Amount)
+	log.Info("MK: opening Mongo session.")
 
 	mongoSession, err := getMongoSession(m.Config)
 	if err != nil {
+		log.Info("MK: error opening Mongo session => " + fmt.Errorf("error connecting to MongoDB: %s", err).Error())
 		return penaltiesData, fmt.Errorf("error connecting to MongoDB: %s", err)
 	}
+
 	defer mongoSession.Close()
+	log.Info("MK: closing Mongo session.")
 	err = mongoSession.DB(m.Config.Database).C(m.Config.LFPCollection).Find(bson.M{"data.created_at": bson.M{
 		"$gt": reconciliationMetaData.StartTime,
 		"$lt": reconciliationMetaData.EndTime,
 	}, "e5_command_error": bson.M{"$ne": ""}}).All(&penalties)
 	if err != nil {
+		log.Info("MK: error retrieving lfp dat => " + fmt.Errorf("error connecting to MongoDB: %s", err).Error())
 		return penaltiesData, fmt.Errorf("error retrieving lfp data: %s", err)
 	}
 
 	penaltiesData = models.PenaltyList{
 		Penalties: penalties,
+	}
+
+	if len(penaltiesData.Penalties) > 0 {
+		log.Info("MK: GetLFPData: len(penalties) => " + strconv.Itoa(len(penalties)))
+		log.Info("MK: GetLFPData: penaltiesData.Penalties[0].CompanyNumber => " + penaltiesData.Penalties[0].CompanyNumber)
+		log.Info("MK: GetLFPData: penaltiesData.Penalties[0].E5CommandError => " + penaltiesData.Penalties[0].E5CommandError)
+		log.Info("MK: GetLFPData: penaltiesData.Penalties[0].Reference => " + penaltiesData.Penalties[0].Reference)
+		log.Info("MK: GetLFPData: penaltiesData.Penalties[0]..Data.Payment.Amount => " + penaltiesData.Penalties[0].Data.Payment.Amount)
 	}
 
 	return penaltiesData, err
