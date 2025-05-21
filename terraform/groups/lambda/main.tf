e0224049d300113b683150a22ef7353d3d7cb562
pshdev0 <pshdev0@gmail.com>
2025-05-21 14:49:57 +0100

    cc-1578 add required providers

--- terraform/groups/lambda/main.tf
+++ terraform/groups/lambda/main.tf
@@ -1,13 +1,27 @@
-provider "aws" {
-  region = var.aws_region
-}
-
 terraform {
+  required_version = ">= 1.3.0, < 2.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.72.0, < 6.0"
+    }
+    vault = {
+      source  = "hashicorp/vault"
+      version = ">= 3.18.0, < 5.0"
+    }
+  }
+
   backend "s3" {
     encrypt = true
   }
 }
 
+provider "aws" {
+  region = var.aws_region
+}
+
+# PH: this is same as in efs-doc-proc so is ok
 provider "vault" {
   auth_login {
     path = "auth/userpass/login/${var.vault_username}"
@@ -15,8 +29,11 @@ provider "vault" {
       password = var.vault_password
     }
   }
+  address = ""
 }
 
+# PH: we should not be using remote state - this is not best practice
+# (see my note here: https://companieshouse.atlassian.net/wiki/spaces/~642153ecf1b529dfa98e4afc/pages/5150343173/Terraform+Lambda+Module#PR-Review)
 data "terraform_remote_state" "network_remote_state" {
   backend = "s3"
   config = {
@@ -26,6 +43,9 @@ data "terraform_remote_state" "network_remote_state" {
   }
 }
 
+# PH: related to the above, get rid of this and
+# PH: this relates back to the last convo I had with Jamie (see locals.tf in efs-doc-proc)
+# PH: basically,
 locals {
   test_and_development_vpc_id     = data.terraform_remote_state.network_remote_state.outputs.vpc_id
   test_and_development_subnet_ids = split(",", data.terraform_remote_state.network_remote_state.outputs.application_ids)
